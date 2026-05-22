aa5447616fa8350b4f817daee8697dab294d1dd9
oriioll <oriolplazas2@gmail.com>
2026-05-22 18:14:03 +0200

    feat: add session logic to header

--- frontend/scripts/includes.js
+++ frontend/scripts/includes.js
@@ -1,4 +1,6 @@
-const header = `
+import { userIsLogged } from "./api/api";
+const isLoggedIn = false;
+let header = `
 <a href="/frontend/pages/veure-productes.html" class="headerTitle">ProductCycle </a>
 <nav class="headerNav">
 <a class="headerLinks" href="/frontend/pages/veure-productes.html">Productes disponibles</a>
@@ -6,9 +8,33 @@ const header = `
 <a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
 </nav>
 `;
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const headerContainer = document.getElementById("header");
-  headerContainer.innerHTML = header;
+  try {
+    const data = await userIsLogged();
+    if (data.isLoggedIn === true) {
+      header = `<a href="/frontend/pages/veure-productes.html" class="headerTitle">ProductCycle </a>
+<nav class="headerNav">
+<a class="headerLinks" href="/frontend/pages/veure-productes.html">Productes disponibles</a>
+<a class="headerLinks" href="/frontend/pages/gestionar-productes.html">Gestionar productes</a>
+<a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
+<button class="logout" id="logout">Log Out</button>
+</nav>`;
+    } else if (data.error) {
+      throw new Error("Error fetching API");
+    } else if (data.isLoggedIn === false) {
+      header = `<a href="/frontend/pages/veure-productes.html" class="headerTitle">ProductCycle </a>
+<nav class="headerNav">
+<a class="headerLinks" href="/frontend/pages/veure-productes.html">Productes disponibles</a>
+<a class="headerLinks" href="/frontend/pages/gestionar-productes.html">Gestionar productes</a>
+<a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
+<button class="login" id="login">Log Out</button>
+</nav>`;
+    }
+    headerContainer.innerHTML = header;
+  } catch (e) {
+    headerContainer.innerHTML = header;
+  }
 });
 
 document.addEventListener("scroll", () => {
