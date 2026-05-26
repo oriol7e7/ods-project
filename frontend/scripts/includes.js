9df58636a3a2b1b4971a14919ff7bf30e2ab1565
oriioll <oriolplazas2@gmail.com>
2026-05-26 21:19:33 +0200

    feat: add darkmode feature to page

--- frontend/scripts/includes.js
+++ frontend/scripts/includes.js
@@ -1,5 +1,6 @@
 import { userIsLogged } from "./api/api.js";
 const isLoggedIn = false;
+let darkModeVar = localStorage.getItem("darkmode") === "true";
 /**
  * Clear user session and reload the page
  * @author Oriol Plazas
@@ -17,11 +18,17 @@ let header = `
 <a class="headerLinks" href="/frontend/pages/gestionar-productes.html">Gestionar productes</a>
 <a class="headerLinks" href="/frontend/pages/crear-producte.html">Puja un producte</a>
 <a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
+<svg id="darkmode" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12t2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21m0-2q2.2 0 3.95-1.213t2.55-3.162q-.5.125-1 .2t-1 .075q-3.075 0-5.238-2.163T9.1 7.5q0-.5.075-1t.2-1q-1.95.8-3.163 2.55T5 12q0 2.9 2.05 4.95T12 19m-.25-6.75"/>
+</svg>
 </nav>
 `;
 document.addEventListener("DOMContentLoaded", async () => {
   const headerContainer = document.getElementById("header");
   try {
+    if (darkModeVar === true) {
+      document.documentElement.classList.toggle("dark");
+    }
     const data = await userIsLogged();
     if (data.loggedIn === true) {
       //si esta loguejar surt boto de logout
@@ -33,6 +40,9 @@ document.addEventListener("DOMContentLoaded", async () => {
           <a class="headerLinks" href="/frontend/pages/crear-producte.html">Puja un producte</a>
           <a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
           <button class="logout" id="logout">Log Out</button>
+         <svg id="darkmode" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12t2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21m0-2q2.2 0 3.95-1.213t2.55-3.162q-.5.125-1 .2t-1 .075q-3.075 0-5.238-2.163T9.1 7.5q0-.5.075-1t.2-1q-1.95.8-3.163 2.55T5 12q0 2.9 2.05 4.95T12 19m-.25-6.75"/>
+</svg>
         </nav>
       `;
       //listener per si fa logout
@@ -46,7 +56,10 @@ document.addEventListener("DOMContentLoaded", async () => {
           <a class="headerLinks" href="/frontend/pages/gestionar-productes.html">Gestionar productes</a>
           <a class="headerLinks" href="/frontend/pages/crear-producte.html">Puja un producte</a>
           <a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
-          <button class="login" id="login">Log In</button> 
+          <button class="login" id="login">Log In</button>
+          <svg id="darkmode" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12t2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21m0-2q2.2 0 3.95-1.213t2.55-3.162q-.5.125-1 .2t-1 .075q-3.075 0-5.238-2.163T9.1 7.5q0-.5.075-1t.2-1q-1.95.8-3.163 2.55T5 12q0 2.9 2.05 4.95T12 19m-.25-6.75"/>
+</svg>
         </nav>
       `;
       //listener per si fa login
@@ -63,8 +76,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         <a class="headerLinks" href="/frontend/pages/gestionar-productes.html">Gestionar productes</a>
         <a class="headerLinks" href="/frontend/pages/crear-producte.html">Puja un producte</a>
         <a class="headerLinks" href="/frontend/pages/sostenibilitat.html">Sostenibilitat</a>
-      </nav>
+        <svg id="darkmode" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+  <path fill="currentColor" d="M12 21q-3.75 0-6.375-2.625T3 12t2.625-6.375T12 3q.35 0 .688.025t.662.075q-1.025.725-1.638 1.888T11.1 7.5q0 2.25 1.575 3.825T16.5 12.9q1.375 0 2.525-.613T20.9 10.65q.05.325.075.662T21 12q0 3.75-2.625 6.375T12 21m0-2q2.2 0 3.95-1.213t2.55-3.162q-.5.125-1 .2t-1 .075q-3.075 0-5.238-2.163T9.1 7.5q0-.5.075-1t.2-1q-1.95.8-3.163 2.55T5 12q0 2.9 2.05 4.95T12 19m-.25-6.75"/>
+</svg>
+        </nav>
     `;
+  } finally {
+    const toggleDarkMode = document.getElementById("darkmode");
+    handleDarkMode(toggleDarkMode);
   }
 });
 
@@ -76,3 +95,11 @@ document.addEventListener("scroll", () => {
     headerContainer.classList.remove("scrolled");
   }
 });
+
+const handleDarkMode = (btn) => {
+  btn.addEventListener("click", () => {
+    darkModeVar = !darkModeVar;
+    localStorage.setItem("darkmode", darkModeVar);
+    document.documentElement.classList.toggle("dark");
+  });
+};
