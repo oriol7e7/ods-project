443557b5f056d0269df552ece47501b1da5c3200
oriol7e7 <oriol.plazas.7e7@itb.cat>
2026-05-28 08:29:39 +0200

    feat: control not logged in users cannot contact the sellers

--- frontend/scripts/products/renderProduct.js
+++ frontend/scripts/products/renderProduct.js
@@ -1,5 +1,6 @@
-import { getProductById } from "../api/api.js";
-
+import { getProductById, userIsLogged } from "../api/api.js";
+let logged = false;
+let userData;
 const mainTitle = document.getElementById("productName");
 /**
  * Load product details from URL parameter and render them
@@ -39,7 +40,8 @@ const getUrlId = () => {
  */
 const renderProduct = (product) => {
   mainTitle.textContent = product.name;
-  document.getElementById("product").innerHTML = `
+  if (logged === true) {
+    document.getElementById("product").innerHTML = `
   <img src="${product.img}" alt="Imatge de ${product.name}">
   <article class="productArticle">
     <h2>${product.name}</h2>
@@ -50,14 +52,38 @@ const renderProduct = (product) => {
         <p class="modalityTag">${product.modality}</p>
         <p><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#00a896" class="icon icon-tabler icons-tabler-filled icon-tabler-map-pin"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6" /></svg>${product.location}</p>
     </div>
+    <p class="userEmail">${userData.user.mail}</p>
     <button class="contactBtn" id="${product.id}">Contacta!</button>
   <article>
   `;
-  document.getElementById(product.id).addEventListener("click", () => {
-    window.open(
-      `mailto:${product.email}?subject=M'interessa el teu producte: ${product.name}`,
-    );
-  });
+    document.getElementById(product.id).addEventListener("click", () => {
+      window.open(
+        `mailto:${product.email}?subject=M'interessa el teu producte: ${product.name}`,
+      );
+    });
+  } else {
+    document.getElementById("product").innerHTML = `
+  <img src="${product.img}" alt="Imatge de ${product.name}">
+  <article class="productArticle">
+    <h2>${product.name}</h2>
+    <p>${product.desc}</p>
+    <h5><strong>${product.price}€</strong></h5>
+    <div class="productTags">
+        <p class="productState">${product.state}</p>
+        <p class="modalityTag">${product.modality}</p>
+        <p><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#00a896" class="icon icon-tabler icons-tabler-filled icon-tabler-map-pin"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M18.364 4.636a9 9 0 0 1 .203 12.519l-.203 .21l-4.243 4.242a3 3 0 0 1 -4.097 .135l-.144 -.135l-4.244 -4.243a9 9 0 0 1 12.728 -12.728zm-6.364 3.364a3 3 0 1 0 0 6a3 3 0 0 0 0 -6" /></svg>${product.location}</p>
+    </div>
+  <article>
+  `;
+  }
 };
 
-renderProductPage();
+document.addEventListener("DOMContentLoaded", async () => {
+  userData = await userIsLogged();
+  if (userData.loggedIn === true) {
+    logged = true;
+  } else {
+    logged = false;
+  }
+  renderProductPage();
+});
